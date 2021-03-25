bdd136dc57567d91897438669376aeeb73e2737e
Sam Ito <isamsito@gmail.com>
2021-03-24 19:15:14 -0600

    fix styling

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,34 +1,33 @@
 export function getAppointmentsForDay(state, day) {
   let daysAppointments = [];
-  const currentDay = state.days.find(selectedDay => selectedDay.name === day);
+  const currentDay = state.days.find((selectedDay) => selectedDay.name === day);
 
   if (currentDay) {
-    currentDay.appointments.forEach(appointment => {
+    currentDay.appointments.forEach((appointment) => {
       daysAppointments.push(state.appointments[appointment]);
     });
     return daysAppointments;
   } else {
     return [];
-  };
-};
+  }
+}
 
-export function getInterview(state, interview){
+export function getInterview(state, interview) {
   if (interview) {
-    return { 
+    return {
       student: interview.student,
-      interviewer: state.interviewers[interview.interviewer]
+      interviewer: state.interviewers[interview.interviewer],
     };
   } else {
     return null;
   }
-};
-
+}
 
 export function getInterviewersForDay(state, day) {
   let interviewers = [];
-  const currentDay = state.days.find(selectedDay => selectedDay.name === day);
+  const currentDay = state.days.find((selectedDay) => selectedDay.name === day);
   if (currentDay) {
-    currentDay.interviewers.forEach(interviewerId => {
+    currentDay.interviewers.forEach((interviewerId) => {
       interviewers.push(state.interviewers[interviewerId]);
     });
   }
