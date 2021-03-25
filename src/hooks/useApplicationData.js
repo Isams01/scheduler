f621f4787b82de74c727aea4ebc89675784aa9a5
Sam Ito <isamsito@gmail.com>
2021-03-24 19:19:26 -0600

    Fix formatting

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,108 +1,116 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useApplicationData(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
 
   function getDay(state) {
-    const currentDay = state.days.find(day => day.name===state.day)
+    const currentDay = state.days.find((day) => day.name === state.day);
     return currentDay;
   }
 
   useEffect(() => {
     const requests = {
-      "GET_DAYS": "/api/days",
-      "GET_APPOINTMENTS": "/api/appointments",
-      "GET_INTERVIEWERS": "/api/interviewers"
-    }
+      GET_DAYS: "/api/days",
+      GET_APPOINTMENTS: "/api/appointments",
+      GET_INTERVIEWERS: "/api/interviewers",
+    };
     const daysRequest = axios.get(requests.GET_DAYS);
     const appointmentsRequest = axios.get(requests.GET_APPOINTMENTS);
     const interviewsRequest = axios.get(requests.GET_INTERVIEWERS);
-    Promise.all([daysRequest, appointmentsRequest, interviewsRequest])
-      .then((responses) => {
+    Promise.all([daysRequest, appointmentsRequest, interviewsRequest]).then(
+      (responses) => {
         const days = responses[0].data;
         const appointments = responses[1].data;
         const interviewers = responses[2].data;
-        setState(prev => ({...prev, days: days, appointments: appointments, interviewers: interviewers }));
-      })
-  },[])
+        setState((prev) => ({
+          ...prev,
+          days: days,
+          appointments: appointments,
+          interviewers: interviewers,
+        }));
+      }
+    );
+  }, []);
 
   function bookInterview(id, interview, mode) {
-    return new Promise ((resolve, reject) => {
-    axios.put(`/api/appointments/${id}`, {interview}).then(res => {
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...interview }
-      };
-      let day;
-      let days;
+    return new Promise((resolve, reject) => {
+      axios
+        .put(`/api/appointments/${id}`, { interview })
+        .then((res) => {
+          const appointment = {
+            ...state.appointments[id],
+            interview: { ...interview },
+          };
+          let day;
+          let days;
 
-      if(mode !== "EDIT") {
-        day = {...getDay(state)};
-        day['spots'] = day['spots'] - 1;
-        days = [...state.days];
-        days[days.findIndex(element => element.id === day.id)] = day; 
-      } else {
-        day = {...getDay(state)};
-        days = [...state.days];
-        days[days.findIndex(element => element.id === day.id)] = day;
-      }
-      
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      
-      setState({
-        ...state, 
-        appointments,
-        days
-      });
-      resolve(res);
-    })
-    .catch(err => reject(err));
+          if (mode !== "EDIT") {
+            day = { ...getDay(state) };
+            day["spots"] = day["spots"] - 1;
+            days = [...state.days];
+            days[days.findIndex((element) => element.id === day.id)] = day;
+          } else {
+            day = { ...getDay(state) };
+            days = [...state.days];
+            days[days.findIndex((element) => element.id === day.id)] = day;
+          }
+
+          const appointments = {
+            ...state.appointments,
+            [id]: appointment,
+          };
+
+          setState({
+            ...state,
+            appointments,
+            days,
+          });
+          resolve(res);
+        })
+        .catch((err) => reject(err));
     });
-  };
+  }
 
-  function setDay(day){
-    setState({...state, day});
+  function setDay(day) {
+    setState({ ...state, day });
   }
 
   function deleteInterview(id) {
-    return new Promise ((resolve, reject) => {
-    axios.delete(`/api/appointments/${id}`).then(res => {
-      const appointment = {
-        ...state.appointments[id],
-        interview: null
-      };
-  
-      const day = {...getDay(state)};
-      day['spots'] = day['spots'] + 1;
-      const days = [...state.days];
-      days[days.findIndex(element => element.id === day.id)] = day; 
+    return new Promise((resolve, reject) => {
+      axios
+        .delete(`/api/appointments/${id}`)
+        .then((res) => {
+          const appointment = {
+            ...state.appointments[id],
+            interview: null,
+          };
 
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      
-      setState({
-        ...state, 
-        appointments,
-        days
-      });
-      resolve(res);
-    })
-    .catch(err => reject(err));
+          const day = { ...getDay(state) };
+          day["spots"] = day["spots"] + 1;
+          const days = [...state.days];
+          days[days.findIndex((element) => element.id === day.id)] = day;
+
+          const appointments = {
+            ...state.appointments,
+            [id]: appointment,
+          };
+
+          setState({
+            ...state,
+            appointments,
+            days,
+          });
+          resolve(res);
+        })
+        .catch((err) => reject(err));
     });
-  };
+  }
 
-
-  return {state, setDay, bookInterview, deleteInterview}
-
+  return { state, setDay, bookInterview, deleteInterview };
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,31 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
-
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode, replace = false) {
+  const transition = function (newMode, replace = false) {
     if (replace) {
       setMode(newMode);
     } else {
       setHistory((prev) => {
-        return [...prev, newMode]
+        return [...prev, newMode];
       });
       setMode(newMode);
     }
-    
-  }
+  };
 
-  const back = function() {
+  const back = function () {
     if (history.length > 1) {
       setHistory((prev) => {
-        const tempHistory = prev.slice(0, prev.length - 1)
+        const tempHistory = prev.slice(0, prev.length - 1);
         setMode(tempHistory.slice(-1)[0]);
         return tempHistory;
       });
     }
-  }
+  };
 
   return { mode, transition, back };
 }
