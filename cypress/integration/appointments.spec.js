1a8873da5be2261992d6fd8fe174e70535451853
Sam Ito <isamsito@gmail.com>
2021-03-24 19:26:02 -0600

    Fix some formatting

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -15,10 +15,10 @@ describe("Appointments", () => {
       .click();
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
-      cy.get("[alt='Sylvia Palmer']").click();
-      cy.contains("Save").click()
-      cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-      cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click()
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
   it("should edit an interview", () => {
